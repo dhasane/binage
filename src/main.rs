f22d5b45ac1513b342974473ff6c35eb6afe3b60
dhasane <danihas@live.com>
2021-01-01 18:29:47 -0500

    El metodo usado previamente era erroneo
    Estaba usando el byte de opacidad de cada pixel, por lo que funcionaba
    incorrectamente segun lo esperado.

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,32 @@ use std::io::{self, prelude::*};
 use std::str;
 
 #[allow(unused_imports)]
-use image::{GenericImage, DynamicImage, GenericImageView, ImageBuffer, Pixel, Rgb, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, ImageBuffer, Pixel, Rgb, Rgba};
 
 #[allow(unused_imports)]
 use std::env;
-
 
 // use crate::color::{self, IntoColor};
 
 // esteganografia
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    // let addr = read_all::<Ipv4Addr>(&args[1]);
+    println!("{}", 161 | 0);
 
-    let nom_img = store_f_in_image(
-        &args[1],
-        &args[2]
-    ).unwrap();
+    // let nom_img = match store_f_in_image(&args[1], &args[2]) {
+    //     Ok(a) => a,
+    //     Err(err) => {
+    //         eprintln!("error: {}", err);
+    //         std::process::exit(1);
+    //     }
+    // };
 
     // println!("{}", nom_img);
 
-    // read_f_in_image(
-    //     &nom_img,
-    //     "esto_es_prueba.txt"
-    // )
+    // read_f_in_image(&nom_img, "esto_es_prueba.txt")
 
-    print_image_pixels(&nom_img)
+    // print_image_pixels(&nom_img)
 
     // let aa : Vec<u32> = (0..50_u32)
     //     .map (|x| x + 1)
@@ -39,7 +38,7 @@ fn main() -> io::Result<()> {
     //     ;
     // println!("{:?}", aa);
 
-    // Ok(())
+    Ok(())
 }
 
 #[allow(dead_code)]
@@ -48,36 +47,35 @@ fn store_f_in_image(img_path: &str, file_path: &str) -> io::Result<String> {
 
     let chunk_size = 0x4000;
 
-    let mut img = DynamicImage::ImageRgba8(
-        match image::open(img_path) {
-            Ok(f) => f.into_rgba8(),
-            Err(err) => {
-                eprintln!("error: {}", err);
-                std::process::exit(1);
-            }
+    let mut img = DynamicImage::ImageRgba8(match image::open(img_path) {
+        Ok(f) => f.into_rgba8(),
+        Err(err) => {
+            eprintln!("error: {}", err);
+            std::process::exit(1);
         }
-    );
+    });
 
     // let rgba = open("path/to/some.png").unwrap().into_rgba();
     // let gray = DynamicImage::ImageRgba8(rgba).into_luma();
 
-    let mut fpos_x : u32 = 0;
-    let mut fpos_y : u32 = 0;
+    let mut fpos_x: u32 = 0;
+    let mut fpos_y: u32 = 0;
 
     loop {
         let mut chunk = Vec::with_capacity(chunk_size);
         let n = std::io::Read::by_ref(&mut file)
             .take(chunk_size as u64)
             .read_to_end(&mut chunk)?;
-        if n == 0 { break; }
+        if n == 0 {
+            break;
+        }
         // println!("{:?}", chunk);
 
         // para cada byte dentro de chunk
         for byte in chunk {
+            println!("{:?} \t {}:{}", byte, fpos_x, fpos_y);
 
-            println!("{:?} \t {}:{}", byte, fpos_x, fpos_y );
-
-            let mut pixel = img.get_pixel(fpos_x , fpos_y);
+            let mut pixel = img.get_pixel(fpos_x, fpos_y);
 
             println!("{:?}", pixel);
 
@@ -88,66 +86,74 @@ fn store_f_in_image(img_path: &str, file_path: &str) -> io::Result<String> {
             // let image::Rgba(data) = *pixel;
             // *pixel = image::Rgba([data[0], data[1], data[2], data[3]]);
 
-            pixel[3] = byte;
-
-            img.put_pixel(
-                fpos_x,fpos_y,
-                // Rgba<u8>::from([pixel[0], pixel[1],pixel[2], byte])
-                pixel
-            );
-
-            println!("{:?}", pixel);
-
-            if fpos_x > img.width() {
-                fpos_x = 0;
-                fpos_y += 1;
-            } else {
-                fpos_x += 1;
+            println!("{:?}", byte.to_be_bytes());
+            for bit in format!("{:#b}", byte)[2..].chars() {
+                println!("{}", bit);
             }
+
+            // pixel[0] |= byte;
+            // pixel[1] |= byte;
+            // pixel[2] |= byte;
+
+            // img.put_pixel(
+            //     fpos_x, fpos_y, // Rgba<u8>::from([pixel[0], pixel[1],pixel[2], byte])
+            //     pixel,
+            // );
+
+            // println!("{:?}", pixel);
+
+            // if fpos_x > img.width() {
+            //     fpos_x = 0;
+            //     fpos_y += 1;
+            // } else {
+            //     fpos_x += 1;
+            // }
 
             println!();
         }
 
         println!("------------------");
 
-        if n < chunk_size { break; }
+        if n < chunk_size {
+            break;
+        }
     }
 
     println!("{}:{}", fpos_x, fpos_y);
 
-    for y in 0..fpos_y + 1 {
-        for x in 0..fpos_x + 1 {
-            let pixel = img.get_pixel(x , y);
+    // for y in 0..fpos_y + 1 {
+    //     for x in 0..fpos_x + 1 {
+    //         let pixel = img.get_pixel(x, y);
 
-            println!("{:?}", pixel);
-        }
-    }
-    println!("------------------");
-    for y in 0..1 { // img.height() {
-        for x in 0..50 { // img.width() {
-            let pixel = img.get_pixel(x , y);
-            println!("{:?}", pixel);
-        }
-    }
+    //         println!("{:?}", pixel);
+    //     }
+    // }
+    // println!("------------------");
+    // for y in 0..1 {
+    //     // img.height() {
+    //     for x in 0..50 {
+    //         // img.width() {
+    //         let pixel = img.get_pixel(x, y);
+    //         println!("{:?}", pixel);
+    //     }
+    // }
 
     let nombre = format!("copia_esteg_{}", img_path);
 
-    let res = img.save(&nombre);
+    // let res = img.save(&nombre);
+    Ok("hola".to_string())
 
-    Ok(
-        match res {
-            Ok(()) => nombre,
-            Err(err) => {
-                eprintln!("error: {}", err);
-                std::process::exit(1);
-            }
-        }
-    )
+    // Ok(match res {
+    //     Ok(()) => nombre,
+    //     Err(err) => {
+    //         eprintln!("error: {}", err);
+    //         std::process::exit(1);
+    //     }
+    // })
 }
 
 #[allow(dead_code)]
 fn read_f_in_image(img_path: &str, file_path: &str) -> io::Result<()> {
-
     println!("leyendo {}", img_path);
     println!("creando {}", file_path);
 
@@ -163,42 +169,31 @@ fn read_f_in_image(img_path: &str, file_path: &str) -> io::Result<()> {
 
     println!(" tam imagen {}:{}", img.width(), img.height());
 
-    // for y in 0..fpos_y + 1 {
-    //     for x in 0..fpos_x + 1 {
-    //         let pixel = img.get_pixel(x , y);
-    //         println!("{:?}", pixel);
-    //     }
-    // }
     for y in 0..img.height() {
-        let aa : Vec<_> = (0..img.width())
-            .map (
-                |x| {
-                    println!("{}:{}  -> {:?}", x, y, img.get_pixel(x,y));
-                    img.get_pixel(x,y)[3]
-                }
-            )
-            .collect()
-            ;
+        let aa: Vec<_> = (0..img.width())
+            .map(|x| {
+                println!("{}:{}  -> {:?}", x, y, img.get_pixel(x, y));
+                img.get_pixel(x, y)[3]
+            })
+            .collect();
 
         println!("{:?}", aa);
 
-        let pal = str::from_utf8( &aa ).unwrap();
+        let pal = str::from_utf8(&aa).unwrap();
         println!("{}", pal);
 
         // println!("{:?}", aa);
 
         match file.write(&aa) {
-        Ok(f) => f,
-        Err(err) => {
-            eprintln!("error: {}", err);
-            std::process::exit(1);
-        }
-    };
-
+            Ok(f) => f,
+            Err(err) => {
+                eprintln!("error: {}", err);
+                std::process::exit(1);
+            }
+        };
     }
 
     Ok(())
-
 }
 
 // Loads an entire file of ip addresses as a Vector of Result<Ipv4Addr> structs
@@ -210,13 +205,8 @@ fn read_f_in_image(img_path: &str, file_path: &str) -> io::Result<()> {
 //         .collect()
 // }
 
-// fn read_bin(mut bin:BinaryReader) {
-//     let read_value = bin.read_bytes().expect("Failed to write f32");
-//     println!("{:?}", read_value);
-// }
-
 #[allow(dead_code)]
-fn read_image(img_path: &str)  {
+fn read_image(img_path: &str) {
     println!("imagen : {}", img_path);
 
     let mut img: image::DynamicImage = match image::open(img_path) {
@@ -239,9 +229,9 @@ fn read_image(img_path: &str)  {
 
     for x in 0..img.width() {
         for y in 0..img.height() {
-            let mut pixel: Rgba<u8> = img.get_pixel(x,y); // .get_pixel_mut(x, y);
+            let mut pixel: Rgba<u8> = img.get_pixel(x, y); // .get_pixel_mut(x, y);
             pixel = image::Rgba([pixel[0], pixel[1], pixel[2], 0]);
-            img.put_pixel(x,y,pixel);
+            img.put_pixel(x, y, pixel);
         }
     }
 
@@ -260,9 +250,11 @@ fn print_image_pixels(img_path: &str) -> io::Result<()> {
         }
     };
 
-    for y in 0..1 { // img.height() {
-        for x in 0..50 { // img.width() {
-            let pixel = img.get_pixel(x , y);
+    for y in 0..1 {
+        // img.height() {
+        for x in 0..50 {
+            // img.width() {
+            let pixel = img.get_pixel(x, y);
             println!("{:?}", pixel);
         }
     }
